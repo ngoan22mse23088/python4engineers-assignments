bfe0081f881bf5560d8cceef8eb5ff0cacb3d4c4
Le Chi Ngoan <lcngoan0607@gmail.com>
2023-04-05 22:30:44 +0700

    update content

--- Assignments 3/database.py
+++ Assignments 3/database.py
@@ -1,10 +1,13 @@
+# Thêm các thư viện cơ bản để xử lý
 import pymysql
 from post import Post
 
 class Database:
+    # Hàm kết nối
     def connect(self):
         return pymysql.connect(host="localhost", user="root", password="12345678", database="mydatabase", charset='utf8mb4')
 
+    # Hàm đọc data table
     def read(self, id):
         con = Database.connect(self)
         cursor = con.cursor()
@@ -22,26 +25,29 @@ class Database:
         finally:
             con.close()
 
+    # Hàm get data từ table posts
     def get_posts(self):
         con = Database.connect(self)
         cursor = con.cursor()
 
         try:
             cursor.execute("SELECT id, title, content FROM posts")
-            data = cursor.fetchall()
-            return [Post(*post) for post in data]
+            posts = cursor.fetchall()
+            return [Post(*post) for post in posts]
         except:
             return ()
         finally:
             con.close()
         pass
 
+    # Hàm get post theo id
     def get_post_by_id(self, id):
         con = Database.connect(self)
         cursor = con.cursor()
 
         try:
-            cursor.execute("SELECT id, title, content FROM posts WHERE id = %s", (id))
+            cursor.execute(
+                "SELECT id, title, content FROM posts WHERE id = %s", (id))
             post = cursor.fetchone()
             return Post(post[0], post[1], post[2])
         except:
@@ -50,6 +56,7 @@ class Database:
             con.close()
         pass
 
+    # Hàm thêm mới 1 post
     def insert(self, post):
         con = Database.connect(self)
         cursor = con.cursor()
@@ -67,6 +74,7 @@ class Database:
         finally:
             con.close()
 
+    # Hàm cập nhật 1 post
     def update(self, post):
         con = Database.connect(self)
         cursor = con.cursor()
@@ -84,6 +92,7 @@ class Database:
         finally:
             con.close()
 
+    # Hàm xóa 1 post theo id
     def delete(self, id):
         con = Database.connect(self)
         cursor = con.cursor()
--- Assignments 3/main.py
+++ Assignments 3/main.py
@@ -1,49 +1,63 @@
+# Thêm các thư viện cơ bản để xử lý
 from post import Post
 from flask import Flask, flash, render_template, request
 from database import Database
-
-app = Flask(__name__)
-app.secret_key = "assignment2"
-db = Database()
-
 import mysql.connector
 
-# Kết nối đến database
-# mydb = mysql.connector.connect(
-#   host="localhost",
-#   user="root",
-#   password="12345678",
-#   database="mydatabase"
-# )
+app = Flask(__name__)
+app.secret_key = "assignment3"
+db = Database()
 
-# Tạo database
-# mycursor = mydb.cursor()
-# mycursor.execute("CREATE TABLE posts (id int NOT NULL AUTO_INCREMENT,title varchar(255),content varchar(255),PRIMARY KEY (id));")
-# mycursor.execute("SELECT * FROM posts")
-# myresult = mycursor.fetchall()
-# for x in myresult:
-#   print(x)
-# print("completed")
+# Kết nối tới MySQL (port mặc định 3306)
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="12345678",
+    database="mydatabase"
+)
 
+# Tạo table nếu chưa có
+mycursor = mydb.cursor()
+mycursor.execute(
+    "CREATE TABLE IF NOT EXISTS posts (id int NOT NULL AUTO_INCREMENT,title varchar(255),content varchar(255),PRIMARY KEY (id));")
+mycursor.execute("SELECT * FROM posts")
+myresult = mycursor.fetchall()
+
+# Trang main
 @app.route('/')
 def index():
-    
     return render_template('index.html')
 
+# Trang create
 @app.route('/create', methods=['GET'])
 def create():
     return render_template('create.html')
 
+# Trang create mới
+@app.route('/create', methods=['POST'])
+def addPost():
+    form = request.form
+    title = form.get('title')
+    content = form.get('content')
+    post = Post(None, title, content)
+    if db.insert(post):
+        flash("Post has been added")
+    else:
+        flash("Post can not be added")
+    return posts()
+
+# Trang lấy 1 post theo id
 @app.route('/edit/<int:id>/', methods=['GET'])
 def edit(id):
     post = db.get_post_by_id(id)
     return render_template('edit.html', post=post)
 
+# Trang cập nhât 1 post theo id
 @app.route('/edit/<int:id>/', methods=['POST'])
 def update_edit(id):
     form = request.form
     title = form.get('title')
-    content = form.get('age')
+    content = form.get('content')
     post = Post(id, title, content)
     if db.update(post):
         flash("A post has been updated")
@@ -52,12 +66,13 @@ def update_edit(id):
     post = db.get_post_by_id(id)
     return render_template('edit.html', post=post)
 
+# Trang get ds post theo id
 @app.route('/posts', methods=['GET'])
 def posts():
     posts = db.get_posts()
-    print(posts)
-    return render_template('post.html', posts=posts)
+    return render_template('posts.html', posts=posts)
 
+# Trang xóa 1 post theo id
 @app.route('/delete/<int:id>', methods=['POST'])
 def delete(id):
     if db.delete(id):
@@ -66,24 +81,11 @@ def delete(id):
         flash("A post can not be deleted")
     return posts()
 
-
-@app.route('/create', methods=['POST'])
-def addphone():
-    form = request.form
-    title = form.get('title')
-    content = form.get('age')
-    post = Post(None, title, content)
-    if db.insert(post):
-        flash("A post has been added")
-    else:
-        flash("A post can not be added")
-    return posts()
-
-
+# Trang xử lý khi not found
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('error.html')
 
 
 if __name__ == '__main__':
-    app.run(port=8181, host="0.0.0.0")
+    app.run(port=8080, host="0.0.0.0")
